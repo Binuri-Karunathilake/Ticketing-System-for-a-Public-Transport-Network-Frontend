b244d2fcc22db94322cb96eab66db0448fd9caad
Tharindu Samarakoon <tharusamara@gmail.com>
2022-11-01 02:22:47 +0530

    Added getBusRoutes backend connection

--- frontend/src/components/BusRouteTable/BusRoute.jsx
+++ frontend/src/components/BusRouteTable/BusRoute.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-const BusRoute = ({ route }) => {
-  route.name = "A to B";
-  route.ticketPrice = 250;
-  route.id = 1;
-  route.stopList = "Kandy,Peradeniya,Kadugannawa,Mawanella";
-
+const BusRoute = ({ route, index }) => {
   return (
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Rs.250</td>
+      <th scope="row">{index + 1}</th>
+      <td>{route.name}</td>
+      <td>Rs.{route.ticketPrice}</td>
       <td>
         <button className="btn btn-warning me-3">More Info</button>
         <button className="btn btn-danger">Delete</button>
