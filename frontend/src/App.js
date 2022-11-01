fc79a60a021915148f2e65687df170cac060340a
Tharindu Samarakoon <tharusamara@gmail.com>
2022-11-02 04:06:11 +0530

    Added admin dashborad prop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import { Route, Routes } from 'react-router-dom';
 import BusTypesService from './services/BusTypesService';
 import AddBusRoute from './components/AddBussRoute/AddBusRoute';
 import RouteDetails from './components/RouteDetails/RouteDetails';
+import AdminDashborad from './components/AdminDashboard/AdminDashborad';
 
 function App() {
   return (
     <div className="container">
       <Routes>
+        <Route path='/' element={<AdminDashborad/>} />
         <Route path='/busRoutes' element={<BusRooutesTable/>} />
         <Route path='/busTypes' element={<BusTypesService/>} />
         <Route path='/busRoutes/add' element={<AddBusRoute/>} />
