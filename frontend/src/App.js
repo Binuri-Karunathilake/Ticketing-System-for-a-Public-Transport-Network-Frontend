6b8fac75dd153847573fc5e1bc23cbd7dd00bee4
Binuri-Karunathilake <binurikarunathilake@gmail.com>
2022-11-02 03:51:03 +0530

    Made changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import{BrowserRouter as Router}from 'react-router-dom';
-import { Routes,Route } from 'react-router';
+// import { Routes,Route } from 'react-router';
 import './App.css';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
-import ListBustTypesComponent from './components/ListBustTypesComponent';
-import AddBusTypesComponent from './components/AddBusTypesComponent';
 import BusRooutesTable from './components/BusRouteTable/BusRoutesTable';
 import { Route, Routes } from 'react-router-dom';
-import BusTypesService from './services/BusTypesService';
 import AddBusRoute from './components/AddBussRoute/AddBusRoute';
 import RouteDetails from './components/RouteDetails/RouteDetails';
+import AddBusType from './components/AddBusType/AddBusTypesComponent';
+import BusTypesTable from './components/BusTypesTable/BusTypesTable';
 
 function App() {
   return (
     <div className="container">
       <Routes>
+        {/*Bus Routes*/}
         <Route path='/busRoutes' element={<BusRooutesTable/>} />
-        <Route path='/busTypes' element={<BusTypesService/>} />
         <Route path='/busRoutes/add' element={<AddBusRoute/>} />
         <Route path='/busRoutes/details' element={<RouteDetails/>} />
+        {/*Bus Types*/}        
+        <Route path='/BusTypes' element={<BusTypesTable/>} />
+        <Route path='/BusTypes/add' element={<AddBusType/>} />        
       </Routes>
     </div>
   );
