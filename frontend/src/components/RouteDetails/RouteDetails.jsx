52838e4f5ef5d3349dc9992dec8e8aed0ba55fa4
Tharindu Samarakoon <tharusamara@gmail.com>
2022-11-01 20:01:19 +0530

    Added new route and route details

--- frontend/src/components/RouteDetails/RouteDetails.jsx
+++ frontend/src/components/RouteDetails/RouteDetails.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import StopDetails from "./StopDetails";
 
 const RouteDetails = () => {
-  const stopList = [
+  const [stopList, setStopList] = useState([
     "Kandy",
     "Mahaiyawa",
     "Mavilmada",
@@ -11,31 +14,67 @@ const RouteDetails = () => {
     "Polgolla",
     "Madawala",
     "Wattegama",
-  ];
+  ]);
+
+  const location = useLocation();
+
+  const { route } = location.state;
+
+  const [routeDetails, setRouteDetails] = useState({
+    name: "",
+    ticketPrice: 0,
+    stopList: "",
+  });
+
+  useEffect(() => {
+    if (route) {
+      console.log(route);
+      setRouteDetails(route);
+      setStopList(routeDetails.stopList.split(","));
+    }
+  }, [route, routeDetails]);
+
+  console.log(route);
 
   return (
     <div>
       <div className="row bg-grey h-100" style={{ height: "100%" }}>
         <div className="col">
           <div className="row">
-            <div className="col-12 p-3">
-              <div className="card rounded-0 shadow">
-                <div className="card-body">
-                  <table className="table ">
-                    <tr>
-                      <td>Route name :</td>
-                      <td>Kandy to Wattegama</td>
-                    </tr>
-                    <tr>
-                      <td>Ticket Price :</td>
-                      <td>Rs.150</td>
-                    </tr>
-                  </table>
-                </div>
+            <div class="card">
+              <div className="card-header">Bus Route Details</div>
+              <img
+                style={{ width: "100%", height: 300, objectFit: "cover" }}
+                src="https://images.pexels.com/photos/5259420/pexels-photo-5259420.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="busStop"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
               </div>
             </div>
           </div>
           <div className="row">
+            <div className="col-md-4 col-sm-6 p-3">
+              <div className="col-12 p-3">
+                <div className="card rounded-0 shadow">
+                  <div className="card-body">
+                    <table className="table ">
+                      <tr>
+                        <td>Route name : </td>
+                        <td>{routeDetails.name}</td>
+                      </tr>
+                      <tr>
+                        <td>Ticket Price :</td>
+                        <td>Rs.{routeDetails.ticketPrice}</td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div
               className="col-md-8 col-sm-6 p-3 my-auto"
               style={{ height: "100%" }}
@@ -56,13 +95,6 @@ const RouteDetails = () => {
                   </tbody>
                 </table>
               </div>
-            </div>
-            <div className="col-md-4 col-sm-6 p-3">
-              <img
-                style={{ width: "100%" }}
-                src="https://images.pexels.com/photos/5259420/pexels-photo-5259420.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="busStop"
-              />
             </div>
           </div>
         </div>
