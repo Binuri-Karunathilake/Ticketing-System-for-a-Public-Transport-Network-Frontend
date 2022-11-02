0ccea11d4ce2476cb234c5b482febf57829c177e
Tharindu Samarakoon <tharusamara@gmail.com>
2022-11-02 20:46:03 +0530

    Login intergration : )

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,12 +13,13 @@ import AddBusType from './components/AddBusType/AddBusTypesComponent';
 import BusTypesTable from './components/BusTypesTable/BusTypesTable';
 import BeginBusJourney from './components/BeginBusJourney/BeginBusJourney';
 import BuyTicket from './components/BuyTicket/BuyTicket';
+import Login from './components/Login/login';
 
 function App() {
   return (
     <div className="container">
       <Routes>
-        <Route path='/' element={<AdminDashborad/>} />
+        <Route path='/Dashboard' element={<AdminDashborad/>} />
         {/*Bus Routes*/}
         <Route path='/busRoutes' element={<BusRooutesTable/>} />
         <Route path='/busRoutes/add' element={<AddBusRoute/>} />
@@ -29,6 +30,8 @@ function App() {
         {/* Ticketing System         */}
         <Route path='/BusJourney' element={<BeginBusJourney />} />
         <Route path='/BusJourney/ticket' element={<BuyTicket />} />
+        {/* Ticketing System         */}
+        <Route path='/' element={<Login />} />
       </Routes>
     </div>
   );
