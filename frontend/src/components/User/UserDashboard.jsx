c733bc453399bfd851cb7f6a4eab5deadc5c8691
Tharindu Samarakoon <tharusamara@gmail.com>
2022-11-11 04:45:48 +0530

    Added user QR

--- frontend/src/components/User/UserDashboard.jsx
+++ frontend/src/components/User/UserDashboard.jsx
@@ -24,21 +24,33 @@ const UserDashborad = () => {
       <br></br>
       <br></br>
 
-
-    <div className="MDBROW" style={{paddingTop:'1%' ,paddingLeft:'4%' ,paddingRight:'4%'}}>
-        
-        <MDBRow  style={{paddingTop:'4%' ,paddingLeft:'4%' ,paddingRight:'4%'}}>
-
-        <MDBCol sm='3'>
-        <MDBCard className=" " style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
-          <MDBCardHeader className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center" style={{color:'white'}}>
-            <MDBIcon fas icon="fa-solid fa-route fa-lg text-black" /><br/> <span></span>
-         </MDBCardHeader>
-         <Link to="/" class="btn btn-primary">
-          Bus Types
-            </Link>
-       </MDBCard>
-        </MDBCol>
+      <div
+        className="MDBROW"
+        style={{ paddingTop: "1%", paddingLeft: "4%", paddingRight: "4%" }}
+      >
+        <MDBRow
+          style={{ paddingTop: "4%", paddingLeft: "4%", paddingRight: "4%" }}
+        >
+          <MDBCol sm="3">
+            <MDBCard
+              className=" "
+              style={{
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              }}
+            >
+              <MDBCardHeader
+                className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center"
+                style={{ color: "white" }}
+              >
+                <MDBIcon fas icon="fa-solid fa-route fa-lg text-black" />
+                <br /> <span></span>
+              </MDBCardHeader>
+              <Link to="/" class="btn btn-primary">
+                Bus Types
+              </Link>
+            </MDBCard>
+          </MDBCol>
 
           {/* <div class="card-header">Routes</div>
           <div class="card-body">
@@ -50,17 +62,28 @@ const UserDashborad = () => {
            
           </div> */}
 
-        <MDBCol sm='3'>
-        <MDBCard className=" " style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
-          <MDBCardHeader className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center" style={{color:'white'}}>
-            <MDBIcon fas icon="plus-circle text-black" />&nbsp;
-            <MDBIcon fas icon="fa-solid fa-route fa-lg text-black" /><br/>
-         </MDBCardHeader>
-         <Link to="/" class="btn btn-primary">
-         Online Ticket
-            </Link>
-       </MDBCard>
-        </MDBCol>
+          <MDBCol sm="3">
+            <MDBCard
+              className=" "
+              style={{
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              }}
+            >
+              <MDBCardHeader
+                className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center"
+                style={{ color: "white" }}
+              >
+                <MDBIcon fas icon="plus-circle text-black" />
+                &nbsp;
+                <MDBIcon fas icon="fa-solid fa-route fa-lg text-black" />
+                <br />
+              </MDBCardHeader>
+              <Link to="/UserReadQR" class="btn btn-primary">
+                Online Ticket
+              </Link>
+            </MDBCard>
+          </MDBCol>
 
           {/* <div class="card">
           <div class="card-header">Featured</div>
@@ -75,17 +98,27 @@ const UserDashborad = () => {
             </Link>
           </div>
         </div> */}
-         <MDBCol sm='3'>
-        <MDBCard className=" " style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
-          <MDBCardHeader className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center" style={{color:'white'}}>
-            <MDBIcon fas icon="fa-solid fa-bus fa-lg text-black" /><br/> <span></span>
-         </MDBCardHeader>
-         <Link to="/" class="btn btn-primary">
-         Safety
-            </Link>
-       </MDBCard>
-        </MDBCol>
-        {/* <div class="card">
+          <MDBCol sm="3">
+            <MDBCard
+              className=" "
+              style={{
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              }}
+            >
+              <MDBCardHeader
+                className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center"
+                style={{ color: "white" }}
+              >
+                <MDBIcon fas icon="fa-solid fa-bus fa-lg text-black" />
+                <br /> <span></span>
+              </MDBCardHeader>
+              <Link to="/" class="btn btn-primary">
+                Safety
+              </Link>
+            </MDBCard>
+          </MDBCol>
+          {/* <div class="card">
           <div class="card-header">Featured</div>
           <div class="card-body">
             <h5 class="card-title">Special title treatment</h5>
@@ -98,18 +131,29 @@ const UserDashborad = () => {
             </Link>
           </div>
         </div> */}
-        <MDBCol sm='3'>
-        <MDBCard className=" " style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
-          <MDBCardHeader className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center" style={{color:'white'}}>
-          <MDBIcon fas icon="plus-circle  text-black" />&nbsp;
-          <MDBIcon fas icon="fa-solid fa-bus fa-lg text-black" /><br/><span></span>
-         </MDBCardHeader>
-         <Link to="/BusTypes/add" class="btn btn-primary">
-         Time Table
-            </Link>
-       </MDBCard>
-        </MDBCol>
-       
+          <MDBCol sm="3">
+            <MDBCard
+              className=" "
+              style={{
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              }}
+            >
+              <MDBCardHeader
+                className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center"
+                style={{ color: "white" }}
+              >
+                <MDBIcon fas icon="plus-circle  text-black" />
+                &nbsp;
+                <MDBIcon fas icon="fa-solid fa-bus fa-lg text-black" />
+                <br />
+                <span></span>
+              </MDBCardHeader>
+              <Link to="/BusTypes/add" class="btn btn-primary">
+                Time Table
+              </Link>
+            </MDBCard>
+          </MDBCol>
 
           {/* <div class="card">
           <div class="card-header">Featured</div>
@@ -124,35 +168,60 @@ const UserDashborad = () => {
             </Link>
             </div>
           </div> */}
-<MDBCol sm='3'>
-        <MDBCard className=" " style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
-          <MDBCardHeader className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center" style={{color:'white'}}>
-          <MDBIcon fas icon="plus-circle  text-black" />&nbsp;
-          <MDBIcon fas icon="fa-solid fa-bus fa-lg text-black" /><br/><span></span>
-         </MDBCardHeader>
-         <Link to="/" class="btn btn-primary">
-         About Us
-            </Link>
-       </MDBCard>
-        </MDBCol>
+          <MDBCol sm="3">
+            <MDBCard
+              className=" "
+              style={{
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              }}
+            >
+              <MDBCardHeader
+                className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center"
+                style={{ color: "white" }}
+              >
+                <MDBIcon fas icon="plus-circle  text-black" />
+                &nbsp;
+                <MDBIcon fas icon="fa-solid fa-bus fa-lg text-black" />
+                <br />
+                <span></span>
+              </MDBCardHeader>
+              <Link to="/" class="btn btn-primary">
+                About Us
+              </Link>
+            </MDBCard>
+          </MDBCol>
 
-        <MDBCol sm='3'>
-        <MDBCard className=" " style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
-          <MDBCardHeader className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center" style={{color:'white'}}>
-          <MDBIcon fas icon="plus-circle  text-black" />&nbsp;
-          <MDBIcon fas icon="fa-solid fa-bus fa-lg text-black" /><br/><span></span>
-         </MDBCardHeader>
-         <Link to="/BusTypes/add" class="btn btn-primary">
-         How to Pay
-            </Link>
-       </MDBCard>
-        </MDBCol>
-
+          <MDBCol sm="3">
+            <MDBCard
+              className=" "
+              style={{
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              }}
+            >
+              <MDBCardHeader
+                className=" fw-bold mt-2 h1 pl-2 pt-5 pb-4 text-center"
+                style={{ color: "white" }}
+              >
+                <MDBIcon fas icon="plus-circle  text-black" />
+                &nbsp;
+                <MDBIcon fas icon="fa-solid fa-bus fa-lg text-black" />
+                <br />
+                <span></span>
+              </MDBCardHeader>
+              <Link to="/BusTypes/add" class="btn btn-primary">
+                How to Pay
+              </Link>
+            </MDBCard>
+          </MDBCol>
         </MDBRow>
       </div>
-      <Footer/>
+      <br />
+      <div className="mt-5">
+        <Footer />
+      </div>
     </div>
-    
   );
 };
 
