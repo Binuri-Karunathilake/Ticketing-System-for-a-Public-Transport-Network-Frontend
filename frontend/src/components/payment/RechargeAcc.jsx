e3f772969d6e7c6e69fd82145170e4c7e2765be4
Adeesha <86665415+it20020644@users.noreply.github.com>
2022-11-12 01:35:13 +0530

    amo amo designer

--- frontend/src/components/payment/RechargeAcc.jsx
+++ frontend/src/components/payment/RechargeAcc.jsx
@@ -68,7 +68,9 @@ const RechargeAcc = ({ type }) => {
           <div className="card-body pt-0">
             <div className="row">
               <div className="col">
-                <form className="row g-3" >
+                <div class="container">
+  <div class="row">
+    <div class="col-sm-8">                <form className="row g-3" >
                   <div className="col-md-6">
                   <label for="ticketPrice" className="form-label">
                     Select card type
@@ -106,7 +108,13 @@ const RechargeAcc = ({ type }) => {
                     <p> Note : Minimum amount to recharge is Rs.500. <br></br>Transaction fee of 2% will be collected towards payment gateway charges.</p>
 
                   </div>
-                </form>
+                </form></div>
+    <div class="col-sm-4"><div class="card" style={{width: 500}}>
+  <img src="https://images.pexels.com/photos/205353/pexels-photo-205353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card-img-top" style={{overflow:"hidden"}}></img>
+ 
+</div></div>
+  </div>
+</div>
               </div>
             </div>
           </div>
