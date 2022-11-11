fbcaafb7c5ae313631d3ce969f1d8f1cbb7dbd3b
Tharindu Samarakoon <tharusamara@gmail.com>
2022-11-11 19:19:05 +0530

    Buy ticket function

--- frontend/src/components/BusTypesTable/BusTypes.jsx
+++ frontend/src/components/BusTypesTable/BusTypes.jsx
@@ -1,31 +1,28 @@
-import Swal
- from "sweetalert2";
+import Swal from "sweetalert2";
 import BusTypesService from "../../services/BusTypesService";
-const BusType = ({BusType, index})=>{
-      const deleteBusType = async() => {
-        try {
-            Swal.fire({
-              title: "Are you sure?",
-              text: "You won't be able to revert this!",
-              icon: "warning",
-              showCancelButton: true,
-              confirmButtonColor: "#3085d6",
-              cancelButtonColor: "#d33",
-              confirmButtonText: "Yes, delete it!",
-            }).then(async(result)=>{
-                if(result.isConfirmed){
-                    const reply = await BusTypesService.deleteBusType(BusType.id);
-                    console.log(reply);
-                    Swal.fire("Deleted!", "Your file has been deleted.", "success");  
-                }
+const BusType = ({ BusType, index }) => {
+  const deleteBusType = async () => {
+    try {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const reply = await BusTypesService.deleteBusType(BusType._id);
+          console.log(reply);
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+          window.location.reload(false);
+        }
+      });
+    } catch (error) {}
+  };
 
-            });
-        } catch (error){}
-    };
-
-
-    
-return (
+  return (
     <tr>
       <th scope="row">{index + 1}</th>
       <td>{BusType.name}</td>
