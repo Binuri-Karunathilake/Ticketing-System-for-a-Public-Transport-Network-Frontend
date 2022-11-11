0d95146ce6e7dc46c9d413852b926091e796da42
Adeesha <86665415+it20020644@users.noreply.github.com>
2022-11-12 00:29:15 +0530

    change about us

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -45,7 +45,7 @@ const aboutUs = () => {
 </header>
 
 <main class="px-3">
-  <h1>About Us!</h1>&nbsp;&nbsp;&nbsp;
+  <h1>About Us!</h1>&nbsp;&nbsp;
   <p class="lead"><h4 style={{textAlign: "center"}}>Welcome To <span id="W_Name1">Journy.lk</span></h4>
     <p style={{textAlign: "center"}}><span id="W_Name2">Journy.lk</span> is a online <span id="W_Type1">ticketing</span> Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of <span id="W_Type2">ticketing</span>, with a focus on dependability and <span id="W_Spec">ticketing
    </span>. We're working to turn our passion for <span id="W_Type3">ticketing</span> into a booming <a href="https://www.blogearns.com/2021/05/free-about-us-page-generator.html" rel="do-follow" style={{ color: "inherit", textDecoration: "none"}}>online website</a>. We hope you enjoy our <span id="W_Type4">ticketing</span> as much as we enjoy offering them to you.</p>
